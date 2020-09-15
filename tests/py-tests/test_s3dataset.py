6990d08d9a3692c058b6b4a0e3e094b01d7da925
Daiming Yang <ydaiming@amazon.com>
2020-09-16 00:19:07 +0800

    add tests for multi download and disabled multi download.

--- tests/py-tests/test_s3dataset.py
+++ tests/py-tests/test_s3dataset.py
@@ -8,7 +8,7 @@ import boto3
 
 
 def test_list_files_prefix():
-    #default region is us-west-2
+    # default region is us-west-2
     s3_dataset_path = 's3://ydaiming-test-data2/test_0/test'
     result1 = list_files(s3_dataset_path)
     s3 = boto3.resource('s3')
@@ -49,6 +49,34 @@ def test_regions(region, s3_dataset_path, bucket_name, prefix):
     del os.environ['AWS_REGION']
 
 
+def test_multi_download(s3_dataset_path, bucket_name, prefix):
+    if 'S3_DISABLE_MULTI_PART_DOWNLOAD' in os.environ:
+        del os.environ['S3_DISABLE_MULTI_PART_DOWNLOAD']
+    result1 = list_files(s3_dataset_path)
+    s3 = boto3.resource('s3')
+    test_bucket = s3.Bucket(bucket_name)
+    result2 = []
+    for url in test_bucket.objects.filter(Prefix=prefix):
+        result2.append('s3://' + url.bucket_name + '/' + url.key)
+    assert isinstance(result1, list)
+    assert isinstance(result2, list)
+    assert result1 == result2
+
+
+def test_disable_multi_download(s3_dataset_path, bucket_name, prefix):
+    os.environ['S3_DISABLE_MULTI_PART_DOWNLOAD'] = "ON"
+    result1 = list_files(s3_dataset_path)
+    s3 = boto3.resource('s3')
+    test_bucket = s3.Bucket(bucket_name)
+    result2 = []
+    for url in test_bucket.objects.filter(Prefix=prefix):
+        result2.append('s3://' + url.bucket_name + '/' + url.key)
+    assert isinstance(result1, list)
+    assert isinstance(result2, list)
+    assert result1 == result2
+    del os.environ['S3_DISABLE_MULTI_PART_DOWNLOAD']
+
+
 def test_file_exists(bucket_name, object_name):
     result1 = file_exists(bucket_name, object_name)
     s3 = boto3.resource('s3')
@@ -76,3 +104,7 @@ test_file_exists('ydaiming-test-data2', 'test_0.JPEG')
 test_file_exists('ydaiming-test-data2', 'test_new_file.JPEG')
 test_file_exists('ydaiming-test-data2', 'folder_1')
 test_get_file_size('ydaiming-test-data2', 'test_0.JPEG')
+test_multi_download('s3://ydaiming-test-data2/test_0/test',
+                    'ydaiming-test-data2', 'test_0/test')
+test_disable_multi_download('s3://ydaiming-test-data2/test_0/test',
+                            'ydaiming-test-data2', 'test_0/test')
