bea8ef960434da457191349afafa46dd88e4b986
Donna Choi <choidong@amazon.com>
2020-10-26 18:32:59 +0000

    Add iterable dataset

--- tests/py-tests/test_hdf5.py
+++ tests/py-tests/test_hdf5.py
@@ -3,59 +3,77 @@ import io
 import boto3
 import numpy as np
 from random import sample
-from awsio.python.lib.io.s3.s3dataset import S3Dataset, list_files
+from time import time
+
+from awsio.python.lib.io.s3.s3dataset import S3Dataset, S3IterableDataset
 import h5py # must be version 2.9.0 or newer
 
-S3_PATH = 's3://choidong-bert/phase1/training/wiki_books_corpus_training'
-S3_FILE_LIST = list_files(S3_PATH)
-KEYS = ['input_ids', 'input_mask', 'segment_ids', 'masked_lm_positions', 'masked_lm_ids', 'next_sentence_labels']
-
-def generate_data_s3dataset(idx):
-    """
-    Return list of numpy arrays created by pulling the idx-th hdf5 file
-    using S3Dataset.
-    """
-    data_file = S3_FILE_LIST[idx]
-    s3_dataset = S3Dataset(data_file)
-    filename, fileobj = s3_dataset[0]
-
-    dataset = io.BytesIO(fileobj)
+def fileobj_to_np_list(fileobj, is_s3=True):
+    KEYS = ['input_ids', 'input_mask', 'segment_ids', 'masked_lm_positions', 'masked_lm_ids', 'next_sentence_labels']
+    if is_s3:
+        fileobj = io.BytesIO(fileobj)
     data_file = []
-    with h5py.File(dataset, 'r') as f:
+    with h5py.File(fileobj, 'r') as f:
         data_file = [np.asarray(f[key][:]) for key in KEYS]
     return data_file
 
-def generate_data_boto(idx):
+def generate_data_boto3(filename):
     """
     Return list of numpy arrays created by first saving the idx-th hdf5 file
     locally using boto3.
     """
-    data_file = S3_FILE_LIST[idx]
-
     # inputs for boto3 s3 download
-    bucket = data_file.split('/')[2]
-    key = os.path.join(*data_file.split('/')[3:])
-    filename = '/tmp/input_file.hdf5'
+    bucket = filename.split('/')[2]
+    key = os.path.join(*filename.split('/')[3:])
+    output_filename = '/tmp/input_file.hdf5'
 
     # download file from s3
     s3 = boto3.client('s3')
-    s3.download_file(bucket, key, filename)
+    s3.download_file(bucket, key, output_filename)
 
     # create list of numpy arrays
-    f = h5py.File(filename, 'r')
-    data_file = [np.asarray(f[key][:]) for key in KEYS]
-    f.close()
-    return data_file
+    return fileobj_to_np_list(output_filename, is_s3=False)
 
-def test_hdf5_to_numpy():
+def test_s3dataset(s3_path, num_samples=5):
+    s3_dataset = S3Dataset(s3_path)
     # pick five random files to test
-    random_idx = sample(range(0, len(S3_FILE_LIST)), 5)
+    random_idx = sample(range(0, len(s3_dataset)), num_samples)
+    total_time = 0
     for idx in random_idx:
-        print("Testing file", S3_FILE_LIST[idx])
-        s3_result = generate_data_s3dataset(idx)
-        boto3_result = generate_data_boto(idx)
+        start = time()
+        filename, fileobj = s3_dataset[idx]
+        print("Testing S3Dataset with file", filename)
+        s3_result = fileobj_to_np_list(fileobj)
+        end = time()
+        total_time += end - start
+        boto3_result = generate_data_boto3(filename)
         assert all([np.allclose(s3_elem, boto3_elem) for s3_elem, boto3_elem in zip(s3_result, boto3_result)])
+    print("Average time for S3Dataset :", total_time / num_samples)
+
+def test_s3iterable(s3_path, num_samples=5):
+    start = time()
+    dataset = S3IterableDataset(s3_path, shuffle_urls=True)
+    end = time()
+    total_time = end - start
+    print("Time to initialize S3IterableDataset :", total_time)
+    tmp = 0
+    for data in dataset:
+        # test only 5 random files
+        tmp += 1
+        if tmp > num_samples:
+            break
+        start = time()
+        filename, fileobj = data
+        print("Testing S3IterableDataset with file", filename)
+        s3_result = fileobj_to_np_list(fileobj)
+        end = time()
+        total_time += end - start
+        boto3_result = generate_data_boto3(filename)
+        assert all ([np.allclose(s3_elem, boto3_elem) for s3_elem, boto3_elem in zip (s3_result, boto3_result)])
+    print("Average time for S3IterableDataset :", total_time / num_samples)
 
 if __name__ == '__main__':
-    test_hdf5_to_numpy()
+    s3_path = 's3://choidong-bert/phase1/training/wiki_books_corpus_training'
+    test_s3dataset(s3_path)
+    test_s3iterable(s3_path)
 
