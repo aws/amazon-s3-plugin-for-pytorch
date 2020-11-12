304bd54b0b5f67b2716b7f7e232f5cc5ed8bb7bb
Zeeshan Ashraf <shaikh.zeeshan.ashraf@gmail.com>
2020-11-12 01:17:37 +0000

    Adds func to read all files at a prefix using boto

--- tests/py-tests/test_integration.py
+++ tests/py-tests/test_integration.py
@@ -54,44 +54,46 @@ import math
 import boto3
 from torch.utils.data import DataLoader
 
-def read_using_boto(bucket, prefix):
+def read_using_boto(bucket, prefix_list):
         fs = io.BytesIO()
         s= boto3.client('s3')
-        s.download_fileobj(bucket,
-                            prefix,
-                            fs)
-        file_content = fs.getvalue()
 
         s3_obj_set = set()
 
-        if prefix[-3:] == "tar":
-            tarfile = tardata(file_content)
-            for fname, content in tarfile:
-                s3_obj_set.add((fname, content))
-                
-        elif prefix[-3:] == "zip":
-            zipfile = zipdata(file_content)
-            for fname, content in zipfile:
-                s3_obj_set.add((fname, content))
+        for prefix in prefix_list:
+            s.download_fileobj(bucket,
+                                prefix,
+                                fs)
+            file_content = fs.getvalue()
 
-        else:
-            s3_obj_set.add((prefix.split("/")[-1]), file_content)
-        
+            if prefix[-3:] == "tar":
+                tarfile = tardata(file_content)
+                for fname, content in tarfile:
+                    s3_obj_set.add((fname, content))
+                    
+            elif prefix[-3:] == "zip":
+                zipfile = zipdata(file_content)
+                for fname, content in zipfile:
+                    s3_obj_set.add((fname, content))
+
+            else:
+                s3_obj_set.add((prefix.split("/")[-1], file_content))
         return s3_obj_set
 
+def get_file_list(bucket, files_prefix):
+    s3 = boto3.resource('s3')
+    my_bucket = s3.Bucket(bucket)
+
+    file_list = [summary.key for summary in my_bucket.objects.filter(Prefix=files_prefix)]
+    return file_list[1:]
 
 
-def test_S3IterableDataset(bucket, prefix_list):
-    s3_obj_set_boto = set()
-    for prefix in prefix_list:
-        s3_obj_set_boto.update(read_using_boto(bucket, prefix))
-    print ("read using boto")
-
+def test_S3IterableDataset(boto_obj_set, bucket, prefix_list):
     s3_obj_set = set()
     batch_size = 32
     url_list = ["s3://" + bucket + "/" + prefix for prefix in prefix_list]
     dataset = S3IterableDataset(url_list)
-    expected_batches = math.ceil(len(s3_obj_set_boto)/batch_size)
+    expected_batches = math.ceil(len(boto_obj_set)/batch_size)
 
     for num_workers in [0, 2, 4, 6, 8]:
         dataloader = DataLoader(dataset,
@@ -103,21 +105,23 @@ def test_S3IterableDataset(bucket, prefix_list):
            batch_set = set(map(tuple, zip(fname, fobj)))
            s3_obj_set.update(batch_set)
            num_batches += 1
-        assert s3_obj_set == s3_obj_set_boto, "Test fails for {} workers".format(num_workers)
+        assert s3_obj_set == boto_obj_set, "Test fails for {} workers".format(num_workers)
         print ("All data correctly loaded for s3Iterable dataset for {} workers".format(num_workers))
         assert expected_batches == num_batches, "Data Incorrectly batched for {} workers".format(num_workers)
         print ("Data correctly batched for s3Iterable dataset for {} workers".format(num_workers))
-        
+    
+
+
 
 if __name__ == "__main__":
     print ("Let us get started")
-    url_list = ["s3://mansmane-dev/imagenet_web_dataset/train/imagenet-train-{}.tar".format(str(0).zfill(6))]
     
-    # s3_read_obj = read_using_boto("mansmane-dev", "imagenet_web_dataset/train/imagenet-train-000000.tar")
-    # print (len((s3_read_obj)))
-    # for name, content in s3_read_obj:
-    #     print (name)
-    #     print (content)
-    #     break
+    bucket = "ydaiming-test-data2"
+    tar_prefix_list = ["integration_tests/imagenet-train-000000.tar"]
+    # boto_read_set = read_using_boto(bucket, tar_prefix_list)
+    # test_S3IterableDataset(boto_read_set, bucket, tar_prefix_list)
 
-    test_S3IterableDataset("mansmane-dev", ["imagenet_web_dataset/train/imagenet-train-000000.tar"])
+    files_prefix = "integration_tests/files/"
+    prefix_list = get_file_list(bucket, files_prefix)
+    boto_read_set = read_using_boto(bucket, prefix_list)
+    
