16197de598e2b052b3980ac950814c8a4eab6c09
Mane <mansmane@3c22fbcffb85.ant.amazon.com>
2020-11-20 09:11:55 -0800

    Updates to config

--- benchmarks/cv/mmclassification/tools/commands.sh
+++ benchmarks/cv/mmclassification/tools/commands.sh
@@ -10,48 +10,32 @@ cd /home/ubuntu/aws_io/benchmarks/cv/mmclassification;
 #configs/imagenet/resnet50_io_latency.py \
 #2 &
 
-
-
-
 #PREFETCH_FACTOR=2
-for PREFETCH_FACTOR in {2,4}
+LATENCY='10ms'
+for N_WORKER in {4,6,8}
 do
-  SECONDS=0
-  N_WORKER=4
+  for PREFETCH_FACTOR in {2,4,6,8}
+  do
+    SECONDS=0
 
-  EPOCH_NUM=2
-  WORK_DIR=work_dirs/0_ms_trial0_n_worker${N_WORKER}_pt_1_7_prefetch_${PREFETCH_FACTOR}
-  /home/ubuntu/anaconda3/envs/open-mmlab/bin/python -u -m torch.distributed.launch \
-  --nproc_per_node=8 --master_port=29500 /home/ubuntu/aws_io/benchmarks/cv/mmclassification/tools/train.py \
-  configs/imagenet/resnet50_io_latency.py \
-  --work-dir=$WORK_DIR \
-  --n_workers=$N_WORKER \
-  --n_epochs=$EPOCH_NUM \
-  --prefetch_factor=$PREFETCH_FACTOR \
-  --launcher pytorch
+    EPOCH_NUM=2
+    WORK_DIR=work_dirs/${LATENCY}_trial0_n_worker${N_WORKER}_pt_1_7_prefetch_${PREFETCH_FACTOR}
+    /home/ubuntu/anaconda3/envs/open-mmlab/bin/python -u -m torch.distributed.launch \
+    --nproc_per_node=8 --master_port=29500 /home/ubuntu/aws_io/benchmarks/cv/mmclassification/tools/train.py \
+    configs/imagenet/resnet50_io_latency_${LATENCY}.py \
+    --work-dir=$WORK_DIR \
+    --n_workers=$N_WORKER \
+    --n_epochs=$EPOCH_NUM \
+    --prefetch_factor=$PREFETCH_FACTOR \
+    --launcher pytorch
 
-  end_time=$SECONDS
-  TIME=$((end_time))
-  echo $TIME > "${WORK_DIR}/timetaken"
+    end_time=$SECONDS
+    TIME=$((end_time))
+    echo $TIME > "${WORK_DIR}/timetaken"
 
-  python tools/send_metrics.py 8 ${WORK_DIR} resnet50 ${EPOCH_NUM} \
-  mmclassification/configs/imagenet/resnet50_io_latency.py > ${WORK_DIR}/metrics
+    python tools/send_metrics.py 8 ${WORK_DIR} resnet50 ${EPOCH_NUM} \
+    mmclassification/configs/imagenet/resnet50_io_latency.py > ${WORK_DIR}/metrics
+  done
 done
 
 
-#mkdir  work_dirs/0_ms_trial0_n_worker4_pt_1_7
-#/home/ubuntu/anaconda3/envs/open-mmlab/bin/python -u -m torch.distributed.launch \
-#--nproc_per_node=8 --master_port=29500 /home/ubuntu/aws_io/benchmarks/cv/mmclassification/tools/train.py \
-#configs/imagenet/resnet50_io_latency.py --work-dir=work_dirs/0_ms_trial0_n_worker4_pt_1_7 --n_workers=4 \
-#--launcher pytorch > work_dirs/0_ms_trial0_n_worker4_pt_1_7/log
-#
-#
-#
-#/home/ubuntu/anaconda3/envs/open-mmlab/bin/python -u -m torch.distributed.launch \
-#--nproc_per_node=8 --master_port=29500 /home/ubuntu/aws_io/benchmarks/cv/mmclassification/tools/train.py \
-#configs/imagenet/resnet50_io_latency.py --work-dir=work_dirs/test --n_workers=4 --launcher pytorch
-#
-#
-#/home/ubuntu/anaconda3/envs/open-mmlab/bin/python -u -m torch.distributed.launch \
-#--nproc_per_node=8 --master_port=29500 /home/ubuntu/aws_io/benchmarks/cv/mmclassification/tools/train.py \
-#configs/imagenet/resnet50_io_latency.py --work-dir=work_dirs/test --n_workers=4 --launcher pytorch
