d97ca3d5cffdb51a6fac222427e16fb46270710a
Ubuntu <ubuntu@ip-172-31-15-178.us-west-2.compute.internal>
2020-12-10 16:22:49 +0000

    Adds changes to run Imagenet from S3

--- benchmarks/cv/mmclassification/configs/_base_/default_runtime.py
+++ benchmarks/cv/mmclassification/configs/_base_/default_runtime.py
@@ -1,5 +1,5 @@
 # checkpoint saving
-checkpoint_config = dict(interval=20)
+checkpoint_config = dict(interval=1000)
 # yapf:disable
 log_config = dict(
     interval=100,
--- benchmarks/cv/mmclassification/mmcls/datasets/__init__.py
+++ benchmarks/cv/mmclassification/mmcls/datasets/__init__.py
@@ -4,6 +4,7 @@ from .cifar import CIFAR10, CIFAR100
 from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
                                RepeatDataset)
 from .imagenet import ImageNet
+from .imagenets3 import ImageNetS3
 from .mnist import MNIST, FashionMNIST
 from .samplers import DistributedSampler
 
@@ -11,5 +12,5 @@ __all__ = [
     'BaseDataset', 'ImageNet', 'CIFAR10', 'CIFAR100', 'MNIST', 'FashionMNIST',
     'build_dataloader', 'build_dataset', 'Compose', 'DistributedSampler',
     'ConcatDataset', 'RepeatDataset', 'ClassBalancedDataset', 'DATASETS',
-    'PIPELINES'
+    'PIPELINES', 'ImageNetS3'
 ]
