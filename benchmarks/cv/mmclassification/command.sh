5da9bb1bcb21667a589d8f58b2e98cfa8ed724b3
Ubuntu <ubuntu@ip-172-31-7-9.us-west-2.compute.internal>
2021-01-15 05:16:36 +0000

    removes sampler and changes command for iterdataset

--- benchmarks/cv/mmclassification/command.sh
+++ benchmarks/cv/mmclassification/command.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m torch.distributed.launch --nproc_per_node=8 --nnodes=1 --node_rank=0  tools/train.py configs/imagenet/resnet50_b64fsx.py --work-dir=work_dir/ebr_file --launcher="pytorch" --no-validate
+python -m torch.distributed.launch --nproc_per_node=8 --nnodes=1 --node_rank=0  tools/train.py configs/imagenetfsx/resnet50_b64.py --work-dir=work_dir/ebr_tar --launcher="pytorch" --no-validate
