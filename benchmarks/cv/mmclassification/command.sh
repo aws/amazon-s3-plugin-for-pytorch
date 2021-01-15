ad3fe31fceb52eaac5ca198ff88b3241feaa7421
Ubuntu <ubuntu@ip-172-31-9-114.us-west-2.compute.internal>
2021-01-15 05:11:07 +0000

    Changes command for tar file

--- benchmarks/cv/mmclassification/command.sh
+++ benchmarks/cv/mmclassification/command.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-python -m torch.distributed.launch --nproc_per_node=8 --nnodes=1 --node_rank=0  tools/train.py configs/imagenet/resnet50_b64fsx.py --work-dir=work_dir/ibr_file --launcher="pytorch" --no-validate
+python -m torch.distributed.launch --nproc_per_node=8 --nnodes=1 --node_rank=0  tools/train.py configs/imagenetfsx/resnet50_b64.py --work-dir=work_dir/ibr_tar --launcher="pytorch" --no-validate
